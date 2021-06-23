74782c79700d3d5611b9ae57520905806e4bbd95
SojungLim <82495447+SojungLim@users.noreply.github.com>
2021-06-23 12:27:08 +0900

    second

--- carousel.js
+++ carousel.js
@@ -5,20 +5,20 @@ const firstPageImage = document.querySelector(".middle img");
         let i = 1;
         function clickRightCirclebtn() {
             if (i<5){
-                firstPageImage.src=`images/image-${i+1}.png`;
+                firstPageImage.src=`image-${i+1}.png`;
                 i+=1;
             } else  if (i===5) {
                 i = 1;
-                firstPageImage.src = `images/image-1.png`;
+                firstPageImage.src = `image-1.png`;
              }
          }
 
         function clickLeftCirclebtn() {   
             if (i===1) {
                 i = 5;
-                firstPageImage.src = `images/image-5.png`;
+                firstPageImage.src = `image-5.png`;
             } else if(i<=5) {
-                firstPageImage.src=`images/image-${i-1}.png`;
+                firstPageImage.src=`image-${i-1}.png`;
                 i-=1;
             }
         }
